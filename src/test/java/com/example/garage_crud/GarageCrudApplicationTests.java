ec0c020ea4fe051ecca54f8b7614121bc11c13b7
Carlo <cangelucci@itsmeccanicabruzzo.eu>
2023-06-27 20:10:17 +0200

    nuovo esercizio: garage_crud

---
+++ src/test/java/com/example/garage_crud/GarageCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garage_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
